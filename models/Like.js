76011947722089eed795a7f98394bfaa20aca1aa
eviecutrell <evelynn.cutrell@alaskaair.com>
2024-02-16 09:23:50 -0800

    Profile.js, Post.js, Like.js, and User.js completed

--- models/Like.js
+++ models/Like.js
@@ -1,4 +1,8 @@
+const { db, Model, DataTypes } = require("../db/connection.js");
 
-let Like;
+let Like = db.define("Like", {
+    reactionType: DataTypes.STRING,
+    CreatedAt: DataTypes.STRING
+});
 
 module.exports = Like;
